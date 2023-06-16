1100deae69cf65835503334477457cd994bc621a
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-06-16 14:58:20 -0400

    add signature generator react app and tailwind

--- signature/src/main.jsx
+++ signature/src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SignatureGenerator from "./Signature-Generator";
+import SignatureGenerator from "./SignatureGenerator.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SignatureGenerator />
   </React.StrictMode>

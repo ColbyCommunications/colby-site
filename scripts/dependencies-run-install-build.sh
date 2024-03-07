6d55286cbe3d14d733e70912f6351f89f157c25c
Brandon K. Waltz <bwaltz6@gmail.com>
2024-03-07 10:53:17 -0500

    npm install in root in CI

--- scripts/dependencies-run-install-build.sh
+++ scripts/dependencies-run-install-build.sh
@@ -7,6 +7,10 @@ shopt -s extglob # Turns on extended globbing
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
+# root
+printf "Build root... \n"
+npm install
+
 printf "Build Colby Theme... \n"
 cd web/wp-content/themes/colby-college-theme
 composer install

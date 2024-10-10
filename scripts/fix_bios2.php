94e8f6ff370ce8481c0640eac7216d80819d3cba
Brandon K. Waltz <bwaltz6@gmail.com>
2024-10-10 09:16:39 -0400

    Update fix_bios2.php

--- scripts/fix_bios2.php
+++ scripts/fix_bios2.php
@@ -15,9 +15,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-error_log(json_encode($conn));
+// get affected bios
+$bio_query = "SELECT meta_id, meta_value from wp_postmeta WHERE meta_value REGEXP 'href=\"[“]'";
+$result = $conn->query($bio_query);
+
+// process rows
+$rows = [];
+while($row = $result->fetch_row()) {
+    $rows[] = $row;
+}
+
+// apply fix
+foreach ($rows as $r) {
+    $new_bio = preg_replace('/href="[”]{1,}(.+?)[”]{1,}"/', 'href="$1"', $r[1]);
+    $result = $conn->query("UPDATE wp_postmeta SET meta_value ='".$conn->real_escape_string($new_bio)."' WHERE meta_id = ".$r[0]);
+}
 
 $conn->close();
 
 ?>
-

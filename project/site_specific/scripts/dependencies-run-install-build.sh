3d4a65ba6899cc1b61d6887d41f0be813fb43a10
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2025-10-21 15:36:42 -0400

    fix build script

--- project/site_specific/scripts/dependencies-run-install-build.sh
+++ project/site_specific/scripts/dependencies-run-install-build.sh
@@ -7,10 +7,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 printf "Build Colby Theme..."
-cd web/wp-content/themes/colby-college-theme
+cd web/wp-content/themes/colby-college-theme-inertia
 composer install
 composer dump-autoload
-yarn
-yarn scripts:build
+npm install
+npm run build
 cd -
 
--- scripts/dependencies-run-install-build.sh
+++ scripts/dependencies-run-install-build.sh
@@ -7,19 +7,6 @@ shopt -s extglob # Turns on extended globbing
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-<<<<<<< HEAD
-# root
-printf "Build root... \n"
-npm install
-
-printf "Build Colby Theme... \n"
-cd web/wp-content/themes/colby-college-theme
-composer install
-composer dump-autoload
-yarn
-yarn scripts:build
-cd -
-=======
 printf "Build Colby Theme... \n"
 cd web/wp-content/themes/colby-college-theme
 composer install
@@ -28,7 +15,6 @@ yarn
 yarn scripts:build
 cd -
 
->>>>>>> 82e0edde410a5c06b5004614c5c3d74729cc8d35
 
 # npm install
 shopt -u extglob
